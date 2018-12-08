615ffa83553d8a26bbef82fb22a04dd3b0c66826
BRYTA <abiodunsulaiman694@gmail.com>
2018-12-08 18:21:51 +0100

    Updated database dump file

--- storemanagerdb.sql
+++ storemanagerdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 04, 2018 at 02:16 PM
+-- Generation Time: Dec 08, 2018 at 06:20 PM
 -- Server version: 10.1.30-MariaDB
 -- PHP Version: 7.2.2
 
@@ -41,6 +41,14 @@ CREATE TABLE `products` (
   `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `products`
+--
+
+INSERT INTO `products` (`id`, `name`, `price`, `picture`, `description`, `qty_left`, `date_created`, `expiry_date`, `created_by`, `date_updated`) VALUES
+(2, 'Men\'s Clipper', 5000, NULL, 'Best clipper ever', 5, '2018-12-06 15:09:29', '2023-12-06', NULL, '2018-12-08 15:20:35'),
+(3, 'PS4', 120000, NULL, 'Fun and guaranteed to win', 85, '2018-12-08 16:19:55', '2020-12-08', NULL, '2018-12-08 17:16:51');
+
 -- --------------------------------------------------------
 
 --
@@ -51,11 +59,20 @@ CREATE TABLE `supplies` (
   `id` int(10) UNSIGNED NOT NULL,
   `product_id` int(11) NOT NULL,
   `supplier` varchar(50) NOT NULL,
+  `quantity` int(11) NOT NULL,
   `created_by` int(11) DEFAULT NULL,
   `date_created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `date_updated` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `date_supplied` date DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `supplies`
+--
+
+INSERT INTO `supplies` (`id`, `product_id`, `supplier`, `quantity`, `created_by`, `date_created`, `date_updated`, `date_supplied`) VALUES
+(1, 3, 'Sony', 50, NULL, '2018-12-08 16:19:56', '2018-12-08 16:19:56', NULL),
+(2, 3, 'SONY MAX', 35, NULL, '2018-12-08 17:16:51', '2018-12-08 17:16:51', '0000-00-00');
 
 --
 -- Indexes for dumped tables
@@ -82,13 +99,13 @@ ALTER TABLE `supplies`
 -- AUTO_INCREMENT for table `products`
 --
 ALTER TABLE `products`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `supplies`
 --
 ALTER TABLE `supplies`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
